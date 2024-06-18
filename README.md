5f3bd3ebb5e99cf23612a2901cf024f9c07cf9b5
Garrett Mcclintock <garrettjmcclintock@gmail.com>
2024-06-18 16:10:57 -0700

    Create Repo

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# CFB DuckDB
+
+This repo is an example of how to use dlt to create a data pipeline from an API and store the output to a database. It is configured to save to a DuckDB file which is available in the repo. It also includes a sample GitHub Action script for both backfilling the entire database from Princeton Rutgers 1869-Present as well as a weekly update that refreshes the current season.
+
+## Data Source
+
+The data in this repo is sourced from [collegefootballdata.com](https://collegefootballdata.com). API Keys are available for free. While this project currently covers 24 of the available endpoints, it does not include the endpoints that require a specific game id (though if you want to add that, please feel free!)
+
+## Setup
+
+To get started, clone this repo and in a virtual environment run `pip install requirements.txt`.
+
+You will also need to create a file called `settings.py` in the root of your cloned repo and add `API_KEY='YOUR_KEY_HERE'` as the contents of the file. If you are running a GitHub Action on your clone, add a repository secret named `CFBD_API_KEY` and the existing action config file will create the `settings.py` file at run time.
+
+## Usage
+
+If you just want to query this data, you can attach to the duckdb file in the repo. You can do this use dbeaver or another SQL IDE of your choice (or R or Python etc using standard libraries). Simply clone the main branch of the repo and you'll have an up to date duckdb file that can be used locally without having to rerun any API requests. Many dataviz tools also support DuckDB and you can upload that as well if you want to build out dashboards.
+
+Alternatively, you can easily modify this repo to use [MotherDuck](https://motherduck.com) - a cloud version of DuckDB that gives 10GB of storage and 10 hours of query time a month for free.
+
---
+++ cfb_data.py
@@ -0,0 +1,410 @@
+#%%
+import dlt
+import requests
+from settings import API_KEY
+import sys
+import json
+
+url = 'https://api.collegefootballdata.com/'
+headers = {
+    'Authorization': API_KEY
+}
+
+from datetime import datetime
+
+backfill = len(sys.argv) > 1 and sys.argv[1].lower() == "backfill"
+
+
+def generate_seasons(start_year=1869, backfill=False):
+    current_date = datetime.now()
+    current_year = current_date.year
+
+    # If it's January, use the previous year
+    if current_date.month == 1:
+        end_year = current_year - 1
+    else:
+        end_year = current_year
+
+    if backfill:
+        return list(range(start_year, end_year + 1))
+    else:
+        return [end_year]
+
+# Example usage
+seasons = generate_seasons(backfill=backfill)
+secondary_seasons = generate_seasons(start_year=2002,backfill=backfill)
+
+@dlt.resource(write_disposition='merge',primary_key=('season','week','season_type'))
+def calendar():
+    for season in seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team_id'))
+def records():
+    for season in seasons:
+        endpoint_url = url + 'records'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = r.pop('year')
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','player_id','category','stat_type'))
+def player_season_stats():
+    for season in seasons:
+        endpoint_url = url + 'stats/player/season'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = season
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team','stat_name'))
+def season_stats():
+    for season in seasons:
+        endpoint_url = url + 'stats/season'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def season_stats_advanced():
+    for season in seasons:
+        endpoint_url = url + 'stats/season/advanced'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def ratings_sp():
+    for season in seasons:
+        endpoint_url = url + 'ratings/sp'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = r.pop('year')
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def ratings_srs():
+    for season in seasons:
+        endpoint_url = url + 'ratings/srs'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = r.pop('year')
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def ratings_elo():
+    for season in seasons:
+        endpoint_url = url + 'ratings/elo'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = r.pop('year')
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def ratings_fpi():
+    for season in seasons:
+        endpoint_url = url + 'ratings/fpi'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = r.pop('year')
+        yield response
+
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def ppa_teams():
+    for season in seasons:
+        endpoint_url = url + 'ppa/teams'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','id'))
+def ppa_player_season():
+    for season in seasons:
+        endpoint_url = url + 'ppa/players/season'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','id'))
+def recruiting_players():
+    for season in seasons:
+        endpoint_url = url + 'recruiting/players'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = r.pop('year')
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def recruiting_teams():
+    for season in seasons:
+        endpoint_url = url + 'recruiting/teams'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = r.pop('year')
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def fbs_teams():
+    for season in seasons:
+        endpoint_url = url + 'teams/fbs'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = season
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('season','team'))
+def roster():
+    for season in seasons:
+        endpoint_url = url + 'roster'
+        params = {
+            'year' : season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            r['season'] = season
+        yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('id'))
+def games():
+    for season in seasons:
+        for season_type in ['regular','postseason']:
+            endpoint_url = url + 'games'
+            params = {
+                'year' : season,
+                'seasonType': season_type
+            }
+            response = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('id'))
+def game_media():
+    for season in seasons:
+        for season_type in ['regular','postseason']:
+            endpoint_url = url + 'games/media'
+            params = {
+                'year' : season,
+                'seasonType': season_type
+            }
+            response = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield response
+
+
+@dlt.resource(write_disposition='merge',primary_key=('id'))
+def drives():
+    for season in seasons:
+        for season_type in ['regular','postseason']:
+            endpoint_url = url + 'drives'
+            params = {
+                'year' : season,
+                'seasonType': season_type
+            }
+            response = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield response
+
+@dlt.resource(write_disposition='merge',primary_key=('id'))
+def game_player_stats():
+    for season in seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'games/players'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield game
+
+@dlt.resource(write_disposition='merge',primary_key=('id'))
+def game_team_stats():
+    for season in seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'games/teams'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield game
+
+@dlt.resource(write_disposition='merge',primary_key=('id'))
+def lines():
+    for season in secondary_seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'lines'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield game
+
+@dlt.resource(write_disposition='merge',primary_key=('id','team'))
+def ppa_game_team():
+    for season in secondary_seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'ppa/games'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield game
+
+@dlt.resource(write_disposition='merge',primary_key=('season','name','team','week','season_type'))
+def ppa_game_player():
+    for season in secondary_seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'ppa/players/games'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            for g in game:
+                g['season_type'] = r.get('season_type')
+            yield game
+
+@dlt.resource(write_disposition='merge',primary_key=('id','team'))
+def game_team_stats_advanced():
+    for season in secondary_seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'stats/game/advanced'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield game
+
+@dlt.resource(write_disposition='merge',primary_key=('id'))
+def plays():
+    for season in secondary_seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'plays'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield game
+
+@dlt.resource(write_disposition='merge',primary_key=('season','week','season_type'))
+def rankings():
+    for season in seasons:
+        endpoint_url = url + 'calendar'
+        params = {
+            'year': season
+        }
+        response = requests.get(endpoint_url,params=params,headers=headers).json()
+        for r in response:
+            endpoint_url = url + 'rankings'
+            params = {
+                'year' : season,
+                'seasonType': r.get('season_type'),
+                'week': r.get('week')
+            }
+            game = requests.get(endpoint_url,params=params,headers=headers).json()
+            yield game
+
+# %%
+
+@dlt.source
+def cfbd():
+    # return[calendar,recruiting_teams,recruiting_players,ppa_player_season,
+    #        ppa_teams,ratings_elo,ratings_srs, ratings_sp,season_stats_advanced,season_stats,
+    #        player_season_stats, records,fbs_teams,roster,games,game_media,
+    #        ratings_fpi,drives,lines,plays,rankings,ppa_game_team,ppa_game_player,
+    #        game_player_stats, game_team_stats, game_team_stats_advanced
+    #        ]
+    return[plays,rankings,ppa_game_team,ppa_game_player]
+    
+pipeline = dlt.pipeline(
+    pipeline_name='cfbd',
+    destination='duckdb',
+    dataset_name='ncaafb'
+)
+
+load_info = pipeline.run(cfbd())
+print(load_info)
